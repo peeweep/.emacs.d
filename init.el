2eb1a5dbe96316c418acf75bf89e69da5e3e2dd7
peeweep <peeweep@0x0.ee>
2022-03-01 10:13:53 +0000

    add auto-package-update

--- init.el
+++ init.el
@@ -59,3 +59,14 @@
 ;; format-all
 (use-package format-all
   :bind ("C-c C-f" . format-all-buffer))
+
+;; auto-package-update
+(use-package auto-package-update
+  :if (not (daemonp))
+  :custom
+  (auto-package-update-interval 7) ;; in days
+  (auto-package-update-prompt-before-update t)
+  (auto-package-update-delete-old-versions t)
+  (auto-package-update-hide-results t)
+  :config
+  (auto-package-update-maybe))
